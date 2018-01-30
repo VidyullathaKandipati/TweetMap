41e7a558d1dbd22f5446b0758c7ad20958a876e3
Hen Chow <henchow@gmail.com>
2018-01-30 15:21:26 +1100

    implemented axios call

--- src/tweet-map/components/tweet-map.jsx
+++ src/tweet-map/components/tweet-map.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import geocoder from 'geocoder';
+import $ from "jquery";
+import axios from "axios";
 
 // var geocoder = null;
 
@@ -21,13 +23,14 @@ class TweetMap extends React.Component {
 
     this.state = {
       searchString: '',
-      tweet: 'hello',
       latlng: {lat: '' , lng: ''},
+      tweets: [],
     };
     // TweetMap.getGecoder();
 
     this.onSearchChange = this.onSearchChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.generateMarkers = this.generateMarkers.bind(this);
   }
 
   onSearchChange(event) {
@@ -37,16 +40,43 @@ class TweetMap extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     const tweetMap = this;
-    geocoder.geocode(this.state.searchString, function(err, res) {
-      if (!err) {
-        tweetMap.setState({ latlng: res.results[0].geometry.location });
-        console.log('from values', tweetMap.state);
+    let tweetLocations = [];
+
+    axios.get('http://localhost:5000/tweets', {
+      params: {
+        searchString: this.state.searchString,
       }
+    })
+    .then((res) => {
+      console.log('success', res);
+      res.data.map((tweet) => {
+        geocoder.geocode(tweet.userLocation, function(err, res) {
+          if (!err && res.results) {
+            // tweetMap.setState({ latlng: res.results[0].geometry.location });
+            console.log('from values', res);
+            tweetLocations.push(res.results[0].geometry.location);
+          }
+        });
+      });
+      tweetMap.setState({ tweets: tweetLocations }, () => {
+        console.log(tweetMap.state);
+      });
+    })
+    .catch((err) => {
+      console.log('error', err);
+    });
+  }
+
+  generateMarkers() {
+    return this.state.tweets.map((tweet) => {
+      console.log(tweet);
+      return <Marker position={tweet} />
     });
   }
 
   render() {
     const { searchString, tweet, latlng } = this.state;
+    const markers = this.generateMarkers();
     // const marker = <Marker name={'Current location'} position={latlng} />;
     return(
       <div id="tweet-map">
@@ -56,15 +86,8 @@ class TweetMap extends React.Component {
           <br />
           <input className="submit-button" type="submit" value="Submit" />
         </form>
-        <Map google={this.props.google} zoom={14}>
-
-          <Marker position={latlng} />
-
-          <InfoWindow
-          visible={true}
-          >
-            <h1>{tweet}</h1>
-          </InfoWindow>
+        <Map google={this.props.google} zoom={2}>
+          {markers}
         </Map>
       </div>
     );
@@ -74,5 +97,5 @@ class TweetMap extends React.Component {
 TweetMap.displayName = 'TweetMap';
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDudKcRTkZ1idASKJKKeXA_cC3iDTXa-5I'
+  apiKey: 'AIzaSyAAC1Ywo5pV5DxcHPBjykxnVuzUphxk_r4'
 })(TweetMap)
